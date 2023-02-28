1a4d0ecdcad236ab7fe72cb2825a68830330c8bc
zakiraghayev <57976785+zakiraghayev@users.noreply.github.com>
2023-02-28 13:18:51 +0400

    Create webhook.py

---
+++ webhook.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+# Get arguments from command line
+if len(sys.argv) < 2:
+    print("Please provide message as arguments.")
+    sys.exit(1)
+message = sys.argv[1]
+
+# Define the message payload
+payload = {"text": f"{message}"}
+
+# Define the webhook URL
+url = os.getenv('SLACK_WEBHOOK_URL')
+
+# Send the request
+response = requests.post(url, data=json.dumps(payload), headers={"Content-Type": "application/json"})
+
+# Check if the request was successful
+if response.status_code == 200:
+    print("Message sent successfully!")
+else:
+    print(f"Error sending message: {response.text}")
